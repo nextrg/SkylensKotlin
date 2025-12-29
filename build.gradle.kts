d6c6a513ccfc992aafc44f9521d9d6f0118e7dd1
nextrg <oliwiercz2@wp.pl>
2025-12-29 13:13:25 +0100

    remove resourcefullib from include

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     modImplementation("com.terraformersmc:modmenu:${project.property("modmenu_version")}")
     modImplementation("com.teamresourceful.resourcefullib:resourcefullib-fabric-1.21.9:3.9.0")
     modImplementation("earth.terrarium.olympus:olympus-fabric-1.21.9:1.6.2")
-    include("com.teamresourceful.resourcefullib:resourcefullib-fabric-1.21.9:3.9.0")
     include("earth.terrarium.olympus:olympus-fabric-1.21.9:1.6.2")
 
     modRuntimeOnly("me.djtheredstoner:DevAuth-fabric:1.2.1")
